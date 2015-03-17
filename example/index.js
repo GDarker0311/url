9b8e61a454d9ad36192a746bc8958d141a5b1023
vivaxy <xyxuye2007@126.com>
2015-03-17 18:01:27 +0800

    encode & decode

--- example/index.js
+++ example/index.js
@@ -9,16 +9,22 @@ var log = function (text) {
 var link = 'https://www.test.com:8080/vivaxy/test/url?name=vivaxy&year=2015#some';
 
 var url = new Url(link);
-log('url.href: \n' + url.href);
+log('url.href: ' + url.href);
 
 url.parameter('year', 2014);
 log('url.parameter(\'year\', 2014);');
-log('url.href: \n' + url.href);
+log('url.href: ' + url.href);
 
 url.parameter('month', 6);
 log('url.parameter(\'month\', 6);');
-log('url.href: \n' + url.href);
+log('url.href: ' + url.href);
 
 url.removeParameter('month');
 log('url.removeParameter(\'month\');');
-log('url.href: \n' + url.href);
+log('url.href: ' + url.href);
+
+url.parameter('code', '中文符号测试&=');
+log('url.parameter(\'code\', \'中文符号测试&=\');');
+log('url.href: ' + url.href);
+
+log('url.parameter(\'code\') === \'' + url.parameter('code') + '\'');
