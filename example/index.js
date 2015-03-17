67de2b976809f7304ce873fd893f4a87fcdb1042
vivaxy <xyxuye2007@126.com>
2015-03-17 17:35:56 +0800

    url

--- example/index.js
+++ example/index.js
@@ -9,13 +9,16 @@ var log = function (text) {
 var link = 'https://www.test.com:8080/vivaxy/test/url?name=vivaxy&year=2015#some';
 
 var url = new Url(link);
-
 log('url.href: \n' + url.href);
 
 url.parameter('year', 2014);
-
+log('url.parameter(\'year\', 2014);');
 log('url.href: \n' + url.href);
 
 url.parameter('month', 6);
+log('url.parameter(\'month\', 6);');
+log('url.href: \n' + url.href);
 
+url.removeParameter('month');
+log('url.removeParameter(\'month\');');
 log('url.href: \n' + url.href);
